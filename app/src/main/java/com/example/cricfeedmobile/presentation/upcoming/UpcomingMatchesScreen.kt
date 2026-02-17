3c1ac371f7b8add3b6b2c9b5a1ffcea17a35c28e
samaysn <samay.n@getlokalapp.com>
2026-02-17 15:44:45 +0530

    ADD : List state + UI fix

--- app/src/main/java/com/example/cricfeedmobile/presentation/upcoming/UpcomingMatchesScreen.kt
+++ app/src/main/java/com/example/cricfeedmobile/presentation/upcoming/UpcomingMatchesScreen.kt
@@ -3,6 +3,7 @@ package com.example.cricfeedmobile.presentation.upcoming
 import android.R
 import android.graphics.Paint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,10 +12,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -45,13 +52,23 @@ fun UpcomingMatchesScreen (
 ){
     val upcomingMatches = homeViewModel.upcomingMatchesFlow.collectAsLazyPagingItems()
 
+    val listState = LazyListState()
     Scaffold(
         topBar = {
             TopAppBar(
                 title = { Text("API Test Screen") },
 
+                navigationIcon = {
+                    IconButton(onClick = { onBackClick() }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                }
             )
-        }
+        },
+
     ) { paddingValues ->
         Box(
             modifier = Modifier.padding(
@@ -62,7 +79,9 @@ fun UpcomingMatchesScreen (
         ) {
             LazyColumn(
                 modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(20.dp)
+                verticalArrangement = Arrangement.spacedBy(20.dp),
+                state = listState,
+//                flingBehavior = rememberSnapFlingBehavior(listState)
 
             ) {
                 items(
@@ -78,11 +97,25 @@ fun UpcomingMatchesScreen (
                         modifier = Modifier.fillMaxWidth()
 //                            .background(MaterialTheme.colorScheme.primaryContainer)
                             .height(250.dp)
-                            .padding(horizontal = 20.dp, vertical = 10.dp),
+                            .padding(horizontal = 20.dp, vertical = 10.dp)
+                        ,
+                        colors = CardDefaults.cardColors(
+                            containerColor = Color.White
+                        ),
+                        elevation = CardDefaults.cardElevation(
+                            defaultElevation = 2.dp
+                        ),
+
+
                     ) {
-                        Text(
-                            text = match?.title ?: "NO TITLE"
-                        )
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = match?.title ?: "NO TITLE"
+                            )
+                        }
                     }
                 }
 
